869bc5698454d6fbfaad6c766c05ca6f1a1e7899
Gino Naumann <g@gnaumann.de>
2024-01-11 00:13:06 +0100

    refactor(module): don't specify install dir

--- _modules/acme_sh.py
+++ _modules/acme_sh.py
@@ -13,22 +13,18 @@ from salt.exceptions import CommandExecutionError, SaltInvocationError, CommandN
 
 log = logging.getLogger(__name__)
 
-def _get_acme_bin(
-    script_path,
-    home_dir
-):
+def _get_acme_bin(home_dir):
 
-  acme_bin = script_path if script_path else f"{home_dir}/.acme.sh/acme.sh"
-  if not salt.utils.path.which_bin([acme_bin]):
-    raise CommandNotFoundError("acme.sh script is not available. Specify the absolute script path in `script_path`")
+  script_path = f"{home_dir}/.acme.sh/acme.sh"
 
-  return acme_bin
+  if not salt.utils.path.which_bin([script_path]):
+    raise CommandNotFoundError(f"{script_path} is not available, please install with `acme_sh.install my@example.com`")
+
+  return script_path
 
 def install(
     email,
     user='root',
-    system=False,
-    home=None,
 ):
   """
   Install acme.sh
@@ -39,37 +35,17 @@ def install(
   user
     user to install
     default = root
-
-  system
-    system installation to /opt/acme.sh
-    default = False
-
-  home
-    customized dir to install acme.sh in.
-    When system is False, default: it installs into ~/.acme.sh
   """
-
-  # check input
-  if user != 'root' and system:
-    raise SaltInvocationError("Specify either user or sytem")
 
   # clone https://github.com/acmesh-official/acme.sh
 
-  clone_name = f"acme.sh-{'global' if system else user}"
+  clone_name = f"acme.sh-{user}"
   clone = __salt__["git.clone"]("/tmp", url="https://github.com/acmesh-official/acme.sh", name=clone_name)
 
   if not clone:
     CommandExecutionError("Failed to clone the acme.sh repository")
 
   cmd = ["./acme.sh", "--install", "--accountemail", email, "--nocron"]
-
-  # if system
-  if system:
-    cmd.append("--home /opt/acme.sh")
-  
-  # if user config
-  if user and home:
-    cmd.append(f"--home {home}")
 
   install = __salt__["cmd.run_all"](" ".join(cmd), cwd=f"/tmp/{clone_name}", runas=user)
   __salt__["file.remove"](f"/tmp/{clone_name}")
@@ -78,23 +54,17 @@ def install(
   if install["retcode"] == 0:
     log.debug("acme.sh successfully installed")
   else:
-    return (False, "Installation failed, see logfiles for more information")
+    __context__["retcode"] = 1
+    return "Installation failed, see logfiles for more information"
 
-  if user and not system and not home:
-    install_dir = __salt__["user.info"](user)["home"]
-  elif user and home:
-    install_dir = home
-  else:
-    install_dir = "/opt/acme.sh"
+  install_dir = __salt__["user.info"](user)["home"]
   
   return(f"acme.sh successfully installed in {install_dir}")
 
 
 def register(
     email,
-    script_path=None,
-    run_as='root',
-    cert_path=None
+    user='root'
 ):
   
   """
@@ -104,29 +74,18 @@ def register(
   email
     email to register
 
-  script_path
-    absolute path to acme.sh script
-    default = ~/.acme.sh/acme.sh
-
-  run_as
+  user
     run the command as a specified user
     default: root
-
-  cert_path
-    installation dir of certs
-    default = dirname of `script_path`
   """
 
-  home_dir = __salt__['user.info'](run_as)['home']
+  home_dir = __salt__['user.info'](user)['home']
 
-  acme_bin = _get_acme_bin(script_path, home_dir)
+  acme_bin = _get_acme_bin(home_dir)
 
   cmd = [acme_bin, "--register-account", "-m", email]
 
-  if cert_path:
-    cmd.append(f"--home {cert_path}")
-
-  register = __salt__["cmd.run_all"](" ".join(cmd), python_shell=False, run_as=run_as)
+  register = __salt__["cmd.run_all"](" ".join(cmd), python_shell=False, runas=user)
 
   if register["retcode"] == 0:
     match = re.search(r"ACCOUNT_THUMBPRINT='([^']+)'", register["stdout"])
@@ -148,8 +107,7 @@ def cert(
     dns_plugin=None,
     webroot=None,
     http_port=None,
-    run_as="root",
-    script_path=None,
+    user="root",
     cert_path=None,
     dns_credentials=None,
     force=False
@@ -191,12 +149,9 @@ def cert(
   http_port
     the port the server listen on during standalone or standalone-tls-alpn
 
-  run_as
-    run the command as a specified user, default: root
-
-  script_path
-    absolute path to acme.sh script
-    default = ~/.acme.sh/acme.sh
+  user
+    run the command as a specified user
+    default: root
 
   cert_path
     installation dir of certs
@@ -211,7 +166,7 @@ def cert(
     default = False
   """
 
-  home_dir = __salt__['user.info'](run_as)['home']
+  home_dir = __salt__['user.info'](user)['home']
 
   if acme_mode == "standalone" or acme_mode == "standalone-tls-alpn":
     # check socat is installed, when standalone
@@ -219,7 +174,7 @@ def cert(
       __context__["retcode"] = 1
       return "Install socat to use standalone mode first"
 
-  acme_bin = _get_acme_bin(script_path, home_dir)
+  acme_bin = _get_acme_bin(home_dir)
 
   # check keysize
   possible_keylength = ["ec-256", "ec-384", "ec-521", "2048", "3072", "4096"]
@@ -256,7 +211,7 @@ def cert(
 
   # if cert_path specified
   if cert_path:
-    cmd.extend(["--home", cert_path])
+    cmd.extend(["--cert-home", cert_path])
   else:
     cert_path = f"{home_dir}/.acme.sh"
 
@@ -282,7 +237,7 @@ def cert(
     log.debug("Set dns_credentials as temporary env")
     __salt__["environ.setenv"](dns_credentials)
 
-  issue = __salt__["cmd.run_all"](" ".join(cmd), python_shell=False, run_as=run_as)
+  issue = __salt__["cmd.run_all"](" ".join(cmd), python_shell=False, runas=user)
 
   if issue["retcode"] == 0:
     ret = {
@@ -293,7 +248,7 @@ def cert(
     if issue["stdout"].find("Next renewal time is") != -1:
       ret = f"Certificate in {cert_path}/{name} is valid, re run with `force=True`"
     elif issue["stdout"].find("acme.sh --register-account -m my@example.com") != -1:
-      ret = f"Please register your account: acme_sh.register my@example.com cert_path={cert_path}"
+      ret = f"Please register your account: acme_sh.register my@example.com"
     else:
       ret = issue
 
